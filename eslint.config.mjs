beb391f35e8ae1f599c6b2d30424be107988c8e8
Joe Paolicelli <joe@joepaolicelli.com>
2025-02-22 03:05:22 -0800

    style: update vue/html-self-closing eslint rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,5 +3,9 @@ import withNuxt from './.nuxt/eslint.config.mjs';
 
 export default withNuxt().overrideRules({
   // Your custom configs here
+  'vue/html-self-closing': [
+    'warn',
+    { html: { normal: 'never', void: 'always' } },
+  ],
   'vue/max-attributes-per-line': ['warn', { singleline: 3 }],
 });
