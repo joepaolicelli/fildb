1461773554c42da1c22989f5333b2c3e6145775a
Joe Paolicelli <joe@joepaolicelli.com>
2025-02-28 08:18:49 -0800

    test: make e2e tests produce CTRF report and show in GitHub Actions

--- playwright.config.ts
+++ playwright.config.ts
@@ -19,10 +19,12 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
+  /* Limit the number of failures on CI to save resources */
+  maxFailures: process.env.CI ? 10 : undefined,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [['html'], ['playwright-ctrf-json-reporter', {}]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
