e37b500a7fc0ba0e88dd17f8cb6e7b64dc0bc962
Joe Paolicelli <joe@joepaolicelli.com>
2025-03-05 22:01:19 -0800

    test: should show table of pending products

--- tests/e2e/admin.spec.ts
+++ tests/e2e/admin.spec.ts
@@ -1,7 +1,13 @@
 import { test, expect } from '@playwright/test';
+import { objectToSnake } from 'ts-case-convert';
 
-test.beforeEach(async ({ page }) => {
-  // Setup permissions and roles in DB.
+import { createTestSupabaseClient, getTestUsers, login, prepDb } from './util';
+
+const supabase = createTestSupabaseClient();
+const testUsers = getTestUsers();
+
+test.beforeAll(async () => {
+  await prepDb(supabase);
 });
 
 test.describe('Page Access', () => {
@@ -10,5 +16,45 @@ test.describe('Page Access', () => {
     // Not logged in, so redirected.
     await page.waitForURL('/login');
     expect(page.url().endsWith('/login'));
+  });
+});
+
+test.describe('Pending Products', () => {
+  test('should show table of pending products', async ({ page }) => {
+    await supabase.from('brands').insert(
+      [
+        {
+          id: 'e4e893b8-9197-4b11-82a9-88456a4c11f3',
+          name: 'Test Brand',
+        },
+      ].map(objectToSnake)
+    );
+
+    await supabase.from('products').insert(
+      [
+        {
+          id: '58b064fc-fa2a-4f58-8e62-f0170542311a',
+          name: 'TestProduct1',
+          brandId: 'e4e893b8-9197-4b11-82a9-88456a4c11f3',
+          sources: { sources: [{ url: 'https://example.com' }] },
+        },
+        {
+          id: '9a168708-f953-4e29-a2ea-70106e48cc66',
+          name: 'TestProduct2',
+          brandId: 'e4e893b8-9197-4b11-82a9-88456a4c11f3',
+          sources: { sources: [{ url: 'https://example.com' }] },
+        },
+      ].map(objectToSnake)
+    );
+
+    await login(page, testUsers.testMaintainer1);
+    await page.goto('/admin');
+
+    await expect(
+      page.getByRole('cell', { name: 'TestProduct1' })
+    ).toBeVisible();
+    await expect(
+      page.getByRole('cell', { name: 'TestProduct2' })
+    ).toBeVisible();
   });
 });
