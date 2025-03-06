6430f1cb7792cc2b63186b06664b2a60a1739d0d
Joe Paolicelli <joe@joepaolicelli.com>
2025-03-05 22:00:03 -0800

    test: util functions: createTestSupabaseClient, prepDb, login

--- tests/e2e/login.spec.ts
+++ tests/e2e/login.spec.ts
@@ -1,36 +1,13 @@
 import { test, expect } from '@playwright/test';
-import { createClient } from '@supabase/supabase-js';
 import { objectToSnake } from 'ts-case-convert';
 
-import { getTestUsers } from './util';
+import { createTestSupabaseClient, getTestUsers, prepDb } from './util';
 
-const supabase = createClient(
-  process.env.TEST_SUPABASE_URL ?? '',
-  process.env.TEST_SUPABASE_SECRET_KEY ?? 'placeholder'
-);
+const supabase = createTestSupabaseClient();
 const testUsers = getTestUsers();
 
 test.beforeAll(async () => {
-  // Clear DB.
-  await supabase.rpc('clear_public_schema');
-  // Setup permissions and roles in DB.
-
-  await supabase.from('role_permissions').insert([
-    { role: 'admin', permission: 'manage_brands' },
-    { role: 'admin', permission: 'manage_pages' },
-    { role: 'admin', permission: 'manage_pending_items' },
-    { role: 'admin', permission: 'manage_published_items' },
-    { role: 'maintainer', permission: 'manage_pages' },
-    { role: 'maintainer', permission: 'manage_pending_items' },
-    { role: 'maintainer', permission: 'manage_published_items' },
-  ]);
-  await supabase.from('user_roles').insert(
-    [
-      { userId: testUsers.testAdmin1.userId, role: 'admin' },
-      { userId: testUsers.testMaintainer1.userId, role: 'maintainer' },
-      { userId: testUsers.testMaintainer2.userId, role: 'maintainer' },
-    ].map(objectToSnake)
-  );
+  await prepDb(supabase);
 });
 
 test.describe('Login Form', () => {
--- tests/e2e/util.ts
+++ tests/e2e/util.ts
@@ -1,8 +1,52 @@
+import type { Page } from '@playwright/test';
+import { createClient, type SupabaseClient } from '@supabase/supabase-js';
+import { objectToSnake } from 'ts-case-convert';
+
+export function createTestSupabaseClient() {
+  return createClient(
+    process.env.TEST_SUPABASE_URL ?? '',
+    process.env.TEST_SUPABASE_SECRET_KEY ?? 'placeholder'
+  );
+}
+
+export async function prepDb(supabase: SupabaseClient) {
+  // Clear DB.
+  await supabase.rpc('clear_public_schema');
+
+  // Setup permissions and roles in DB.
+  const testUsers = getTestUsers();
+  await supabase.from('role_permissions').insert([
+    { role: 'admin', permission: 'manage_brands' },
+    { role: 'admin', permission: 'manage_pages' },
+    { role: 'admin', permission: 'manage_pending_items' },
+    { role: 'admin', permission: 'manage_published_items' },
+    { role: 'maintainer', permission: 'manage_pages' },
+    { role: 'maintainer', permission: 'manage_pending_items' },
+    { role: 'maintainer', permission: 'manage_published_items' },
+  ]);
+  await supabase.from('user_roles').insert(
+    [
+      { userId: testUsers.testAdmin1.userId, role: 'admin' },
+      { userId: testUsers.testMaintainer1.userId, role: 'maintainer' },
+      { userId: testUsers.testMaintainer2.userId, role: 'maintainer' },
+    ].map(objectToSnake)
+  );
+}
+
 export type TestUserInfo = {
   email: string;
   password: string;
   userId: string;
 };
+
+export async function login(page: Page, user: TestUserInfo) {
+  await page.goto('/login');
+  await page.getByRole('textbox', { name: 'Email' }).fill(user.email);
+  await page.getByRole('textbox', { name: 'Password' }).fill(user.password);
+  await page.getByRole('button', { name: 'Log In' }).click();
+
+  await page.waitForURL('/admin');
+}
 
 export function getTestUsers() {
   return {
