65e12726dee053105071ac1feac34d472f083421
Joe Paolicelli <joe@joepaolicelli.com>
2025-03-16 01:54:27 -0700

    fix: layout tweaks

--- app/components/SiteHeader.vue
+++ app/components/SiteHeader.vue
@@ -11,7 +11,7 @@ const navItems = ref([
 ]);
 </script>
 <template>
-  <div class="sticky top-0 flex max-w-screen justify-between p-3">
+  <div class="sticky top-0 flex justify-between p-3">
     <div class="self-center text-2xl">
       <b>FilDB</b>
     </div>
--- app/layouts/admin.vue
+++ app/layouts/admin.vue
@@ -6,11 +6,11 @@ const layoutStore = useLayoutStore();
   <div
     class="grid min-h-screen transition-[grid-template-columns] duration-300 ease-in-out"
     :class="{
-      'grid-cols-[0px_auto]': !layoutStore.adminSidebarOpen,
+      'grid-cols-[0px_100vw]': !layoutStore.adminSidebarOpen,
       'grid-cols-[100vw_auto]': layoutStore.adminSidebarOpen,
-      'sm:grid-cols-[240px_auto]': layoutStore.adminSidebarOpen,
-      'lg:grid-cols-[64px_auto]': !layoutStore.adminSidebarOpen,
-      'lg:grid-cols-[240px_auto]': layoutStore.adminSidebarOpen,
+      'sm:grid-cols-[240px_calc(100vw-240px)]': layoutStore.adminSidebarOpen,
+      'lg:grid-cols-[64px_calc(100vw-64px)]': !layoutStore.adminSidebarOpen,
+      'lg:grid-cols-[240px_calc(100vw-240px)]': layoutStore.adminSidebarOpen,
     }"
   >
     <AdminSidebar />
